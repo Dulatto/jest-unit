5d6eeab2b3583994f84f0d819474b732a83a5fe7
Dulatto <dulat.omarov@gmail.com>
2022-06-06 21:19:29 -0400

    multi test

--- src/validateValue/validateValue.test.js
+++ src/validateValue/validateValue.test.js
@@ -3,3 +3,20 @@ const validateValue = require('./validateValue');
 test('Validation', () => {
     expect(validateValue(50)).toBe(true);
 })
+describe('validateValue', () => {
+    test('Correct value', () => {
+        expect(validateValue(50)).toBe(true);
+    })
+    test('Less than correct value', () => {
+        expect(validateValue(-1)).toBe(false);
+    })
+    test('More than correct value', () => {
+        expect(validateValue(101)).toBe(false);
+    })
+    test('Lower border value', () => {
+        expect(validateValue(0)).toBe(true);
+    })
+    test('Upper border value', () => {
+        expect(validateValue(100)).toBe(true);
+    })
+})
