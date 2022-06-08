f7482c17bb5180688f3f2f2ed58b22b3d0d51996
Dulatto <dulat.omarov@gmail.com>
2022-06-07 21:46:06 -0400

    arr

--- src/mapArrToString/mapArrToString.test.js
+++ src/mapArrToString/mapArrToString.test.js
@@ -5,5 +5,14 @@ describe('mapArrToString', () => {
     test('Correct value', () => {
         expect(mapArrToString([1, 2, 3])).toEqual(['1', '2', '3']);
     })
+    test('Mishmash value', () => {
+        expect(mapArrToString([1, 2, 3, null, undefined, 'asfasf'])).toEqual(['1', '2', '3']);
+    })
+    test('Empty array', () => {
+        expect(mapArrToString([])).toEqual([]);
+    })
+    test('negative', () => {
+        expect(mapArrToString([1, 2, 3])).not.toEqual(['1', '2', '3', '4']);
+    })
 
 })
