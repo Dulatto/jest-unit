a48f9521a8f1b6a0477c2c92081fec344880bb91
Dulatto <dulat.omarov@gmail.com>
2022-06-07 22:05:27 -0400

    off

--- src/square/square.js
+++ src/square/square.js
@@ -1,4 +1,11 @@
+// const square = (number) => {
+//     return number * number;
+// }
+
 const square = (number) => {
-    return number * number;
+    if (number === 1) {
+        return 1;
+    }
+    return Math.pow(number, 2)
 }
 module.exports = square;
--- src/square/square.test.js
+++ src/square/square.test.js
@@ -10,10 +10,13 @@ describe('square', () => {
         //add to db
     })
     test('Correct value', () => {
-        expect(square(2)).toBe(4);
-        expect(square(2)).toBeLessThan(5);
-        expect(square(2)).toBeGreaterThan(3);
-        expect(square(2)).not.toBeUndefined();
+        // expect(square(2)).toBe(4);
+        // expect(square(2)).toBeLessThan(5);
+        // expect(square(2)).toBeGreaterThan(3);
+        // expect(square(2)).not.toBeUndefined();
+        const spyMathPow = jest.spyOn(Math, 'pow');
+        square(2)
+        expect(spyMathPow).toBeCalledTimes(1);
     })
     afterEach(() => { // delete from db
     })
